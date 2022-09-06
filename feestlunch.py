b5ee0599f9efd275cff01273c5d42e2b927f3692
Thomas Kooij <9907182@mydavinci.nl>
2022-09-06 09:48:14 +0200

    Feestlunch

---
+++ feestlunch.py
@@ -0,0 +1,17 @@
+croissant = 0.39
+aantal = 17
+totaalcroissant = (croissant*aantal)
+
+stokbrood = 2.78
+aantal = 2
+totaalstokbrood = (stokbrood*aantal)
+
+kortingsbon = -0.50
+aantal = 3
+totaalkortingsbon = (kortingsbon*aantal)
+
+prijs = totaalcroissant+totaalstokbrood
+print(prijs+totaalkortingsbon)
+
+
+
