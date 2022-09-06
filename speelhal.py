42e33befb9cd211730e3d4a130928a4ce8c79daa
Thomas Kooij <9907182@mydavinci.nl>
2022-09-06 10:03:14 +0200

    Speelhal

---
+++ speelhal.py
@@ -0,0 +1,13 @@
+from logging.handlers import TimedRotatingFileHandler
+
+
+personen = 4
+ticket = 7.45
+prijs = personen*ticket
+
+gameseat = 0.37
+tijd = 9
+prijsgame = gameseat*tijd*personen
+
+totaal = print(prijs+prijsgame)
+
