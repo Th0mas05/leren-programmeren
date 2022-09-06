974b6310f0d5b2d67610103fb4ba170587144e1a
Thomas Kooij <9907182@mydavinci.nl>
2022-09-06 11:50:19 +0200

    feestlunch

--- feestlunch.py
+++ feestlunch.py
@@ -1,13 +1,16 @@
 croissant = 0.39
-aantal = 17
+aantal = input("Hoeveel croissants wilt u? ")
+aantal = int(aantal)
 totaalcroissant = (croissant*aantal)
 
 stokbrood = 2.78
-aantal2 = 2
+aantal2 = input("Hoeveel stokbroden wilt u? ")
+aantal2 = int(aantal2)
 totaalstokbrood = (stokbrood*aantal2)
 
 kortingsbon = -0.50
-aantal3 = 3
+aantal3 = input("Hoeveel kortingsbonnen heeft u? ")
+aantal3 = int(aantal3)
 totaalkortingsbon = (kortingsbon*aantal3)
 
 prijs = totaalcroissant+totaalstokbrood
