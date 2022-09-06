cffe76e5833dec2e78fae4c24ce9f763682b522e
Thomas Kooij <9907182@mydavinci.nl>
2022-09-06 12:11:56 +0200

    speelhal

--- speelhal.py
+++ speelhal.py
@@ -1,13 +1,15 @@
 
-personen = 4
+personen = input("Met hoeveel personen bent u? ")
+personen = int(personen)
 ticket = 7.45
 prijs = personen*ticket
 
-gameseat = 0.37
-tijd = 9
+gameseat = 0.074
+tijd = input("Hoe veel minuten wilt u in de SUPER VR GAME SEAT? ")
+tijd = int(tijd)
 prijsgame = gameseat*tijd*personen
 
 totaal = prijs+prijsgame
 
-print("Dit geweldige dagje-uit ",personen, "mensen in de Speelhal met 45 minuten VR kost je maar",totaal, "euro")
+print("Dit geweldige dagje-uit met",personen, "personen in de Speelhal met" ,tijd, "minuten VR kost je maar",totaal, "euro")
 
