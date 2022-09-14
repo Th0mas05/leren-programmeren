b9fa59f971a97a6a1f62f5225276be7dcb086733
Thomas Kooij <9907182@mydavinci.nl>
2022-09-14 11:53:27 +0200

    pizzaCalculator

--- module 2/pizzaCalculator.py
+++ module 2/pizzaCalculator.py
@@ -1,9 +1,8 @@
 # Thomas Kooij, Pizzacalculator
 
-
-small  = 6.99      # Prijs in euro
-medium = 11.95     # Prijs in euro
-large  = 17.45     # Prijs in euro
+small  = 6.99   
+medium = 11.95    
+large  = 17.45     
 
 print("|--------------------------|")
 print("|   small  pizza   €6.99   |")
@@ -11,31 +10,41 @@ print("|   medium pizza   €11.95  |")
 print("|   large  pizza   €17.45  |")
 print("|--------------------------|")
 
-#keuze grootte pizza
-size = input("Kies de grootte van uw pizza: ")
-if  size.lower()  == ("small"): 
-    kaas = 1
-    print("U heeft een small pizza gekozen")
-elif size.lower() == ("medium"): 
-    kaas = 2
-    print("U heeft een medium pizza gekozen")
-elif size.lower() == ("large"): 
-    kaas = 3
-    print("U heeft een large pizza gekozen")
 
-#keuze hoeveelheid pizza's
-amount = input("Hoeveel pizza's wilt u? ")
-amount = int(amount)
+repeat = True
+while repeat == True:
 
-if kaas == 1:
-    totaalprijs = small * amount
-elif  kaas == 2:
-    totaalprijs = medium * amount  
-elif  kaas == 3:
-    totaalprijs = large * amount 
+    size = input("Welke grootte pizza wilt u? ")
+    if size.lower() == "small":
+        kaas = 1
+        repeat = False
+    if size.lower() == "medium":
+        kaas = 2
+        repeat = False
+    if size.lower() == "large":
+        kaas = 3
+        repeat = False
 
-print("|--------------------------               ")
-print("|  " +str(amount)+ "x "+str(size)+" pizza ")
-print("|                                         ")
-print("|--------------------------               ")
-print("| €",totaalprijs,"                        ")
+succes = False
+while succes == False:
+    try:
+        amount = input("Hoeveel pizza's wilt u? ")
+        amount = int(amount)
+        succes = True
+    except:  
+        print("Dit is geen getal")
+
+    if succes == True:
+        if kaas == 1:
+            totaalprijs = small * amount
+        elif  kaas == 2:
+            totaalprijs = medium * amount  
+        elif  kaas == 3:
+            totaalprijs = large * amount 
+
+        print("|--------------------------               ")
+        print("|  " +str(amount)+ "x "+str(size)+" pizza ")
+        print("|                                         ")
+        print("|--------------------------               ")
+        print("| €",totaalprijs,"                        ")
+
