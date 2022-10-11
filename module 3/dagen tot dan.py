f7d8bb973889d0b4f3cd817ece074db39fffb804
Thomas Kooij <9907182@mydavinci.nl>
2022-10-11 09:53:35 +0200

    new

---
+++ module 3/dagen tot dan.py
@@ -0,0 +1,45 @@
+dagen = ('maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag')
+
+dagenvraag = input("welke dag is het? ")
+while dagenvraag == ('maandag'):
+    print ('maandag')
+    break
+while dagenvraag == ('dinsdag'):
+    print ("maandag")
+    print ("dinsdag")
+    break
+while dagenvraag == ('woensdag'):
+    print ("maandag")
+    print ("dinsdag")
+    print ("woensdag")
+    break
+while dagenvraag == ('donderdag'):
+    print ("maandag")
+    print ("dinsdag")
+    print ("woensdag")
+    print ("donderdag")
+    break
+while dagenvraag == ('vrijdag'):
+    print ("maandag")
+    print ("dinsdag")
+    print ("woensdag")
+    print ("donderdag")
+    print ("vrijdag")
+    break
+while dagenvraag == ('zaterdag'):
+    print ("maandag")
+    print ("dinsdag")
+    print ("woensdag")
+    print ("donderdag")
+    print ("vrijdag")
+    print ("zaterdag")
+    break
+while dagenvraag == ('zondag'):
+    print ("maandag")
+    print ("dinsdag")
+    print ("woensdag")
+    print ("donderdag")
+    print ("vrijdag")
+    print ("zaterdag")
+    print ("zondag")
+    break
