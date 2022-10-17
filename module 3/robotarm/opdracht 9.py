83807340866a2bb5552b638e2fab51c21bc3793e
Thomas Kooij <9907182@mydavinci.nl>
2022-10-17 14:20:27 +0200

    aaa

--- module 3/robotarm/opdracht 9.py
+++ module 3/robotarm/opdracht 9.py
@@ -1,11 +1,9 @@
 from RobotArm import RobotArm
-
 robotArm = RobotArm('exercise 9')
-
-# Jouw python instructies zet je vanaf hier:
-robotArm.moveRight()
-robotArm.grab()
-robotArm.drop()
-
-# Na jouw code wachten tot het sluiten van de window:
-robotArm.wait()
+for i in range(4):
+    robotArm.grab()
+    for a in range(6):
+        robotArm.moveRight()
+    robotArm.drop()
+    for b in range(5):
+        robotArm.moveLeft()
