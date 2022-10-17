5efec247721ec08cbad2f03d69afa6b9a54f1155
Thomas Kooij <9907182@mydavinci.nl>
2022-10-17 13:27:33 +0200

    update

--- module 3/robotarm/Opdracht 8.py
+++ module 3/robotarm/Opdracht 8.py
@@ -1,11 +1,15 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 8')
-
 # Jouw python instructies zet je vanaf hier:
 robotArm.moveRight()
-robotArm.grab()
-robotArm.drop()
+for kaas in range(7):
+    robotArm.grab()
+    for x in range(8):
+        robotArm.moveRight()
+    robotArm.drop()
+    for z in range(8):
+        robotArm.moveLeft()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
