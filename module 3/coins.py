b6ce07d9b96a71cd51495d355bcbf161cdcc1794
Thomas Kooij <9907182@mydavinci.nl>
2022-11-02 11:34:26 +0100

    update

--- module 3/coins.py
+++ module 3/coins.py
@@ -14,20 +14,20 @@ nrCoinsReturned5 = 0
 nrCoinsReturned2 = 0
 nrCoinsReturned1 = 0
 
-toPay = int(float(input('Amount to pay: ')))*100 # doet alles * 1 zodat het in euro's is
-paid = int(float(input('Paid amount: ')))*100 # doet alles * 1 zodat het in euro's is
-change = paid - toPay # doet het betaalde bedrag min het bedrag dat je moet betalen
+toPay = int(float(input('Amount to pay: ')))*100      # doet alles * 1 zodat het in euro's is
+paid = int(float(input('Paid amount: ')))*100         # doet alles * 1 zodat het in euro's is
+change = paid - toPay                                 # doet het betaalde bedrag min het bedrag dat je moet betalen
 
-if change > 0: # als het wisselgeld hoger als 0 is
-  coinValue = 500 # is de munten value 500
+if change > 0:                                        # als het wisselgeld hoger als 0 is
+  coinValue = 500                                     # is de munten value 500
   
-  while change > 0 and coinValue > 0: # terwijl wisselgeld hoger als 0 is en eurovalue hoger als 0 is.
-    nrCoins = change // coinValue # wordt change en eurovalue naar beneden afgerond
+  while change > 0 and coinValue > 0:                 # terwijl wisselgeld hoger als 0 is en eurovalue hoger als 0 is.
+    nrCoins = change // coinValue                     # wordt change en eurovalue naar beneden afgerond
 
-    if nrCoins > 0: # als nrcoins is groter als 0
-      print('return maximal',nrCoins,'coins of',coinValue,'cents!' ) # print die hoeveel van de munten je moet betalen
-      nrCoinsReturned = int(input('How many coins of '+ str(coinValue) + ' cents did you return? ')) # vraagt hoeveel je er hebt ingeleverd
-      change -= int(nrCoinsReturned) * coinValue # berekent hoeveel je nog moet betalen als je niet genoeg hebt betaald.
+    if nrCoins > 0:                                   # als nrcoins is groter als 0
+      print('return maximal',nrCoins,'coins of',coinValue,'cents!' )                                      #  print die hoeveel van de munten je moet betalen
+      nrCoinsReturned = int(input('How many coins of '+ str(coinValue) + ' cents did you return? '))      # vraagt hoeveel je er hebt ingeleverd
+      change -= int(nrCoinsReturned) * coinValue                                                           # berekent hoeveel je nog moet betalen als je niet genoeg hebt betaald.
       if change > 0:
         print ("you have not paid the required change")
     
@@ -61,21 +61,21 @@ if change > 0: # als het wisselgeld hoger als 0 is
       coinValue = 0
 
 
-if change > 0: # als wisselgeld groter is als 0 dan zegt die hoeveel je nog moet betalen
+if change > 0:                          # als wisselgeld groter is als 0 dan zegt die hoeveel je nog moet betalen
   print('Change not returned: ', str(change) +' cents')
 else:
   print('done')
 
 # comment on code below: geeft de value's aan hoeveel de munten waard zijn.
-print (' _____________________________________________________________ ')
-print ('|                                                             |')
-print ('| u heeft '+ str(nrCoinsReturned500) +' coins of 5 Euro payed |')
-print ('| u heeft '+ str(nrCoinsReturned200) +' coins of 2 Euro payed |')
-print ('| u heeft '+ str(nrCoinsReturned100) +' coins of 1 Euro payed |')
-print ('| u heeft '+ str(nrCoinsReturned50) +' coins of 50 cent payed |')
-print ('| u heeft '+ str(nrCoinsReturned20) +' coins of 20 cent payed |')
-print ('| u heeft '+ str(nrCoinsReturned10) +' coins of 10 cent payed |')
-print ('| u heeft '+ str(nrCoinsReturned5) +' coins of 5 cent payed |')
-print ('| u heeft '+ str(nrCoinsReturned2) +' coins of 2 cent payed |')
-print ('| u heeft '+ str(nrCoinsReturned1) +' coins of 1 cent payed |')
-print ('|_____________________________________________________________|')
+print (' ____________________________________')
+print ('|                                    ')
+print ('| u heeft '+ str(nrCoinsReturned500) +' coins of 5 Euro payed   ')
+print ('| u heeft '+ str(nrCoinsReturned200) +' coins of 2 Euro payed    ')
+print ('| u heeft '+ str(nrCoinsReturned100) +' coins of 1 Euro payed     ')
+print ('| u heeft '+ str(nrCoinsReturned50) +' coins of 50 cent payed   ')
+print ('| u heeft '+ str(nrCoinsReturned20) +' coins of 20 cent payed   ')
+print ('| u heeft '+ str(nrCoinsReturned10) +' coins of 10 cent payed   ')
+print ('| u heeft '+ str(nrCoinsReturned5) +' coins of 5 cent payed    ')
+print ('| u heeft '+ str(nrCoinsReturned2) +' coins of 2 cent payed    ')
+print ('| u heeft '+ str(nrCoinsReturned1) +' coins of 1 cent payed    ')
+print ('|____________________________________')
