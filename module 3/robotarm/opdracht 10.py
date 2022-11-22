6bebb38f39f1253d26131fe7d4a70230ccd23f07
Thomas Kooij <9907182@mydavinci.nl>
2022-11-22 10:11:12 +0100

    update

--- module 3/robotarm/opdracht 10.py
+++ module 3/robotarm/opdracht 10.py
@@ -2,7 +2,7 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 10')
 
 x = 9
-for f in range(5):
+for blok in range(5):
     robotArm.grab()
     for y in range(x):
      robotArm.moveRight()
