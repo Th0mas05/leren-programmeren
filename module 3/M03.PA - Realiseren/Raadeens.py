e72df0f705243945eca3d58c96f6542214da9bf0
Thomas Kooij <9907182@mydavinci.nl>
2022-11-24 10:54:42 +0100

    update

--- module 3/M03.PA - Realiseren/Raadeens.py
+++ module 3/M03.PA - Realiseren/Raadeens.py
@@ -6,7 +6,7 @@ for raden in range(0, 20):
     getal = random.randint(1,1000)
     
     for vraag in range(0, 10):
-        invoer = int(input("Gok een getal tussen de 1 en de 1000\n\n>> "))
+        invoer = int(input("Gok een getal tussen de 1 en de 1000?\n\n>> "))
         
         if invoer > getal:
             print("Lager!")
@@ -17,11 +17,9 @@ for raden in range(0, 20):
             score = score + 1
             break
         elif invoer > getal - 20 and invoer < getal + 20:
-            print("Je poten staan in de fik zo dichtbij ben je!")
+            print("Je bent warm")
         elif invoer > getal - 50 and invoer < getal + 50:
-            print("De thermostaat slaat op hol, want je bent warm!")
-        
-        print(getal)
+            print("Je bent heel warm")
 
     print()      
     print("Het getal was", getal)
@@ -31,5 +29,3 @@ for raden in range(0, 20):
     if invoer2 == "nee" and raden < 19:
         print("Eindscore:", score, "punt(en)")
         break
-    else:
-        print(score)
