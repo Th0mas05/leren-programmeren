6c738e29f918ad571093b18a33d38b9e9975e5e3
Thomas Kooij <9907182@mydavinci.nl>
2022-11-24 09:47:24 +0100

    Update

---
+++ module 3/M03.D01.O5 - Robotarm/robotarm/opdracht 13.py
@@ -0,0 +1,24 @@
+from RobotArm import RobotArm
+# Let op: hier start het anders voor een random level:
+robotArm = RobotArm()
+robotArm.randomLevel(1,7)
+robotArm.speed = 1
+# Jouw python instructies zet je vanaf hier:
+position = 1
+for i in range(0,9):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color != (""):
+        for i in range(0,position):
+            robotArm.moveRight()
+        robotArm.drop()
+        position += 1
+        for i in range(0,position-1):
+            robotArm.moveLeft()
+    else:
+        robotArm.wait()
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
