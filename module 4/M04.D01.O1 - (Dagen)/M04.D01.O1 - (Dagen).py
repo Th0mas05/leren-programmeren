e38a1f87096199abb4a78565fbc52e349a906de7
Thomas <99071821@mydavinci.nl>
2022-11-26 13:34:47 +0100

    update

--- module 4/M04.D01.O1 - (Dagen)/M04.D01.O1 - (Dagen).py
+++ module 4/M04.D01.O1 - (Dagen)/M04.D01.O1 - (Dagen).py
@@ -1,8 +1,8 @@
 heleweek = ("maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag","zondag")
 
-print(heleweek[0],heleweek[1],heleweek[2],heleweek[3],heleweek[4],heleweek[5],heleweek[6])  # gehele week
-print(heleweek[0],heleweek[1],heleweek[2],heleweek[3],heleweek[4]) # werk week
-print(heleweek[5],heleweek[6]) # weekend dagen
+print(heleweek[:6])  # gehele week
+print(heleweek[:4]) # werk week
+print(heleweek[5:]) # weekend dagen
 
 print()
 
