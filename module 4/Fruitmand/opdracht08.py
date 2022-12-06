f2baee0ed3a01008c719abc26222b107cc9e1619
Thomas Kooij <9907182@mydavinci.nl>
2022-12-06 12:12:46 +0100

    update

--- module 4/Fruitmand/opdracht08.py
+++ module 4/Fruitmand/opdracht08.py
@@ -3,4 +3,4 @@ from fruitmand import fruitmand
 fruitmand.append({'name': 'watermeloen', 'weight': 2000, 'color': 'green', 'round': True})
 
 totaal = fruitmand[0]['weight'] + fruitmand[1]['weight'] + fruitmand[2]['weight'] + fruitmand[3]['weight'] + fruitmand[4]['weight'] + fruitmand[5]['weight'] + fruitmand[6]['weight'] + fruitmand[7]['weight']
-print(totaal)
+print(totaal/1000,"Kg")
