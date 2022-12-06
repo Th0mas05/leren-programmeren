dff5e91ecb95ecaf44af0816136fdf0fc5ea6750
Thomas Kooij <9907182@mydavinci.nl>
2022-12-06 12:11:18 +0100

    update

--- module 4/Fruitmand/opdracht08.py
+++ module 4/Fruitmand/opdracht08.py
@@ -1,9 +1,6 @@
 from fruitmand import fruitmand
 
-a = 0
 fruitmand.append({'name': 'watermeloen', 'weight': 2000, 'color': 'green', 'round': True})
 
-for i in fruitmand:
-   b = fruitmand[i]['weight']
-   a += b
-print(a)
+totaal = fruitmand[0]['weight'] + fruitmand[1]['weight'] + fruitmand[2]['weight'] + fruitmand[3]['weight'] + fruitmand[4]['weight'] + fruitmand[5]['weight'] + fruitmand[6]['weight'] + fruitmand[7]['weight']
+print(totaal)
