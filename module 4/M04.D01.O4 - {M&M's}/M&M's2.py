048093dc194bde5a7351b20b606b8ae15064434b
Thomas Kooij <9907182@mydavinci.nl>
2022-12-06 10:50:42 +0100

    update

--- module 4/M04.D01.O4 - {M&M's}/M&M's2.py
+++ module 4/M04.D01.O4 - {M&M's}/M&M's2.py
@@ -2,17 +2,15 @@ import random
 
 kleur = ["Rood", "Blauw", "Groen", "Geel", "Bruin"]
 zak = {}
-try:
-    keuze = int(input("Hoeveel M&M's wilt U?\n\n >> "))
+keuze = int(input("Hoeveel M&M's wilt U toevoegen? \n\n>> "))
 
-    for i in range(keuze):
-        a = random.randint(0, 5)
-        if kleur[a] not in zak:             # Als de kleur er niet in zit gaat hij automatisch op 1
-            zak.update({kleur[a]: 1})
-        else:
-            x = zak.get(kleur[a]) + 1       # Hij pakt een kleur en kiest er random 1 uit en vervolgens doet hij +1 
-            zak.update({kleur[a]: x})       # vervolgens update hij hem zodat het verschijnt in die print
 
-    print(zak)
-except:
-    print("Dat is geen getal")
+for i in range(keuze):
+    a = random.randint(0, 5)
+    if kleur[a] not in zak:
+        zak.update({kleur[a]: 1})
+    else:
+        x = zak.get(kleur[a]) + 1
+        zak.update({kleur[a]: x})
+
+print(zak)
