4c89f4c826f4c904f91b695b46b7c2586c2d0e90
Thomas <112869433+Th0mas05@users.noreply.github.com>
2023-01-24 14:03:12 +0100

    Update

--- module 5/deel 1/M05.D01.O5 - Fibonacci reeks/fibonacci.py
+++ module 5/deel 1/M05.D01.O5 - Fibonacci reeks/fibonacci.py
@@ -1,14 +1,7 @@
-def Fibonacci(nummer):
-    nul = 0
-    een = 1
-    if nummer == 1:
-        return nul
-    else:
-        print(nul)
-        print(een)
-        for _ in range(2,nummer):
-            multi = nul + een
-            nul = een
-            een = multi
-            print(multi and nul and een) 
-Fibonacci(int(input("Fibonacci reeks, Typ een positief getal hoger dan 5:\n\n>> ")))
+def fibonacci(nummer):
+    nul, een = 0, 1
+    while nul < nummer:
+        print(nul, end=',')
+        nul, een = een, nul+een
+    print()
+fibonacci(35)
