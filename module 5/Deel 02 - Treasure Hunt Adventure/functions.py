957b67aaad312ec881c414c7716e2f325defed12
Thomas <112869433+Th0mas05@users.noreply.github.com>
2023-02-02 11:46:34 +0100

    Update

--- module 5/Deel 02 - Treasure Hunt Adventure/functions.py
+++ module 5/Deel 02 - Treasure Hunt Adventure/functions.py
@@ -1,6 +1,11 @@
 import time
+import math
 from termcolor import colored
 from data import JOURNEY_IN_DAYS
+from data import COST_FOOD_HUMAN_COPPER_PER_DAY
+from data import COST_FOOD_HORSE_COPPER_PER_DAY
+from data import COST_TENT_GOLD_PER_WEEK
+from data import COST_HORSE_SILVER_PER_DAY
 
 ##################### M04.D02.O2 #####################
 
@@ -17,37 +22,50 @@ def platinum2gold(amount:int) -> float:
     return (amount * 25)
 
 def getPersonCashInGold(personCash:dict) -> float:
+    cop2gold = copper2gold(personCash['copper'])
+    silv2gold = silver2gold(personCash['silver'])
+    plat2gold = platinum2gold(personCash['platinum'])
+    kaas = cop2gold + silv2gold + plat2gold
+    personCash = personCash['gold'] + kaas
     return personCash
 
 ##################### M04.D02.O4 #####################
 
 def getJourneyFoodCostsInGold(people:int, horses:int) -> float:
-    pass
+    people_amount_copper = people * COST_FOOD_HUMAN_COPPER_PER_DAY
+    people_amount_copper = people_amount_copper * JOURNEY_IN_DAYS
+    people_amount_gold = copper2gold(people_amount_copper)
+    round(people_amount_gold,2)
+    horse_amount_copper = horses * COST_FOOD_HORSE_COPPER_PER_DAY
+    horse_amount_copper = horse_amount_copper * JOURNEY_IN_DAYS
+    horse_amount_gold = copper2gold(horse_amount_copper)
+    round(horse_amount_gold,2)
+    totaalperdag = horse_amount_gold + people_amount_gold
+    return round(totaalperdag,2)
 
 ##################### M04.D02.O5 #####################
 
 def getFromListByKeyIs(list:list, key:str, value:any) -> list:
-    pass
-
+    return [item for item in list if item[key]== value]
 def getAdventuringPeople(people:list) -> list:
-    pass
+    return getFromListByKeyIs (people,"adventuring",True)
 
 def getShareWithFriends(friends:list) -> int:
-    pass
+    return getFromListByKeyIs (friends,"sharewith",True)
 
 def getAdventuringFriends(friends:list) -> list:
-    pass
+    return getFromListByKeyIs(friends, "adventuring", True)
 
 ##################### M04.D02.O6 #####################
 
 def getNumberOfHorsesNeeded(people:int) -> int:
-    pass
+    return math.ceil(people/2)
 
 def getNumberOfTentsNeeded(people:int) -> int:
-    pass
+    return math.ceil(people/3)
 
 def getTotalRentalCost(horses:int, tents:int) -> float:
-    pass
+    return (horses * silver2gold(COST_HORSE_SILVER_PER_DAY) * JOURNEY_IN_DAYS) + (tents * (COST_TENT_GOLD_PER_WEEK * math.ceil(JOURNEY_IN_DAYS / 7)) )
 
 ##################### M04.D02.O7 #####################
 
