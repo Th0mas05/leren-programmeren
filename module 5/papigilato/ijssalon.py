84f635448d28d4affb9aca0e9789f2e89f68d2dc
Thomas <112869433+Th0mas05@users.noreply.github.com>
2023-03-28 11:35:46 +0200

    ijssalon upgraden

--- module 5/papigilato/ijssalon.py
+++ module 5/papigilato/ijssalon.py
@@ -1,34 +1,48 @@
-print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
+def welcome_message():
+    print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
-while True:
-    aantal_bolletjes = input("Hoeveel bolletjes wilt u? ")
+def ask_bolletjes():
+    bolletjes = input("Hoeveel bolletjes wilt u?\n>> ")
+    if bolletjes.isdigit():
+        bolletjes = int(bolletjes)
+        if bolletjes >= 1 and bolletjes <= 3:
+            return bolletjes
+        elif bolletjes >= 4 and bolletjes <= 8:
+            print(f"Dan krijgt u van mij een bakje met {bolletjes} bolletjes")
+            return bolletjes
+        elif bolletjes > 8:
+            print("Sorry, zulke grote bakken hebben we niet")
+            return ask_bolletjes()
+    else:
+        print("Sorry dat snap ik niet...")
+        return ask_bolletjes()
 
-    if aantal_bolletjes.isdigit():
-        aantal_bolletjes = int(aantal_bolletjes)
-
-        if aantal_bolletjes <= 3:
-            break
-        elif 4 <= aantal_bolletjes <= 8:
-            print(f"Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes.")
-            break
-        else:
-            print("Sorry, zulke grote bakken hebben we niet.")
+def ask_hoorntje_of_bakje(bolletjes):
+    keuze = input(f"Wilt u deze {bolletjes} bolletje(s) in een hoorntje of een bakje?\n>> ")
+    if keuze.lower() == "hoorntje" or keuze.lower() == "bakje":
+        return keuze
     else:
         print("Sorry, dat snap ik niet...")
+        return ask_hoorntje_of_bakje(bolletjes)
 
-while True:
-    bakje_of_hoorntje = input(f"Wilt u deze {aantal_bolletjes} bolletje(s) in een hoorntje of een bakje? ")
-
-    if bakje_of_hoorntje.lower() == "hoorntje" or bakje_of_hoorntje.lower() == "bakje":
-        print(f"Hier is uw {bakje_of_hoorntje.lower()} met {aantal_bolletjes} bolletje(s).")
-        nog_meer_bestellen = input("Wilt u nog meer bestellen? (ja/nee) ")
-
-        if nog_meer_bestellen.lower() == "ja":
-            continue
-        elif nog_meer_bestellen.lower() == "nee":
-            print("Bedankt en tot ziens!")
-            break
-        else:
-            print("Sorry, dat snap ik niet...")
+def serve_ijs(bolletjes, keuze):
+    print(f"Hier is uw {keuze} met {bolletjes} bolletje(s).")
+    antwoord = input("Wilt u nog meer bestellen?\n>> ")
+    if antwoord.lower() == "ja":
+        return True
+    elif antwoord.lower() == "nee":
+        print("Bedankt en tot ziens!")
+        return False
     else:
         print("Sorry, dat snap ik niet...")
+        return serve_ijs(bolletjes, keuze)
+
+def bestel_ijs():
+    welcome_message()
+    while True:
+        bolletjes = ask_bolletjes()
+        keuze = ask_hoorntje_of_bakje(bolletjes)
+        if not serve_ijs(bolletjes, keuze):
+            break
+
+bestel_ijs()
